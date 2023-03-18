cb06297481c4561f64f6f5a941f578d1f3928310
Anubhav <anubhavsharma313@gmail.com>
2023-03-18 05:43:36 -0700

    fix all the glitches

--- tcs-marks-calc.js
+++ tcs-marks-calc.js
@@ -60,49 +60,46 @@ function findTotal(allSectionsAttempts, skipSectionName=null) {
 
 function createSummaryItem(subjectName, subjectMarksDetails, seq = 1) {
     const summaryItem = document.createElement('div');
-    summaryItem.classList.append('summary-item');
+    summaryItem.classList.add('summary-item');
     summaryItem.setAttribute('data-item-type', `accent-${seq}`);
 
     const flexGroup = document.createElement('div');
-    flexGroup.classList.append('flex-group');
+    flexGroup.classList.add('flex-group');
 
-    const summaryIcon =
-        '<svg class="summary-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.25" d="M10.833 8.333V2.5l-6.666 9.167h5V17.5l6.666-9.167h-5Z" /></svg>'
     const summaryItemTitle = document.createElement('h3');
-    summaryItemTitle.classList.append('summary-item-title');
+    summaryItemTitle.classList.add('summary-item-title');
     summaryItemTitle.innerHTML = subjectName
 
-    flexGroup.appendChild(summaryIcon);
-    flexGroup.appendChild(summaryItemTitle);
+    flexGroup.append(summaryItemTitle);
     summaryItem.append(flexGroup);
 
     const summaryScore = document.createElement('p');
-    summaryScore.classList.append('summary-score');
+    summaryScore.classList.add('summary-score');
     const marksObtained = document.createElement('span');
     marksObtained.innerHTML = subjectMarksDetails['Marks Obtained']
     const outOf = document.createElement('span');
     outOf.innerHTML = subjectMarksDetails['Out of']
-    summaryScore.innerHTML = marksObtained + ' / ' + outOf
+    summaryScore.innerHTML = marksObtained.outerHTML + ' / ' + outOf.outerHTML
 
     summaryItem.append(summaryScore);
 
-    return summaryItem
+    return `${summaryItem.outerHTML}`
 }
 
 function createFinalScoreBlock(finalScore) {
-    const content = `<main> <link rel="stylesheet" href="style.css"> <div class="result-summary"> <div class="results grid-flow" data-spacing="large"> <h1 class="section-title">Your Result</h1> <p class="result-score"><span>{{total-marks-obtained}}</span> of {{out-of-total}}</p> <div class="grid-flow"> <p class="result-rank">Great</p> </div> </div> <div class="summary grid-flow" data-spacing="large"> <h2 class="section-title">Summary</h2> <div class="grid-flow">`
+    let content = `<main> <link rel="stylesheet" href="https://anubhav-kaushik.github.io/exams-helper/style.css"> <div class="result-summary"> <div class="results grid-flow" data-spacing="large"> <h1 class="section-title">Your Result</h1> <p class="result-score"><span>{{total-marks-obtained}}</span> of {{out-of-total}}</p> <div class="grid-flow"> <p class="result-rank">Great</p> </div> </div> <div class="summary grid-flow" data-spacing="large"> <h2 class="section-title">Summary</h2> <div class="grid-flow">`
 
     const totalMarksObtained = finalScore['Marks Obtained'];
     const outOfTotal = finalScore['Out of'];
 
-    content.replace('{{total-marks-obtained}}', `${totalMarksObtained}`)
-    content.replace('{{out-of-total}}', `${outOfTotal}`);
+    content = content.replace('{{total-marks-obtained}}', `${totalMarksObtained}`)
+    content = content.replace('{{out-of-total}}', `${outOfTotal}`);
 
     return content
 }
 
 function getFinalTemplate(result) {
-    const firstBlock = createFinalScoreBlock(result['Total']);
+    let firstBlock = createFinalScoreBlock(result['Total']);
 
     let n = 0;
     for (const subjectName in result) {
@@ -223,4 +220,4 @@ const markingScheme = {
 const result = main(page, '.section-cntnr', '.section-lbl', '.rw', markingScheme, 'tier2');
 console.table(result[1]);
 
-// addResultToPage(result[1])
+addResultToPage(result[1])
