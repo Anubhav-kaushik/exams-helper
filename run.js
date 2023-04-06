ba72c53b70a1693f841cf0bfffbe7f69b4270ef3
Anubhav <anubhavsharma313@gmail.com>
2023-04-05 23:49:56 -0700

    update

--- run.js
+++ run.js
@@ -72,6 +72,38 @@ const styleSheets = [
 addStyleSheets(styleSheets);
 addScripts(scripts);
 
+function createTable(data, sortData="rowWise", firstRowHeader=false) {
+    const table = document.createElement("table");
+    table.className = "table";
+    let rowNumber = 0;
+    if (sortData == "rowWise") {
+        for (let row of data) {
+            const row = document.createElement("tr");
+
+            for (let col of row) {
+                let element;
+                if (firstRowHeader && rowNumber == 0) {
+                    element = document.createElement("th");
+                    element.innerHTML = col;
+                } else {
+                    element = document.createElement("td");
+                    element.innerHTML = col;
+                }
+
+                row.append(element);
+            }
+
+            table.append(row);
+
+            rowNumber++;
+        }
+    } else if (sortData == "columnWise") {
+        null
+    }
+
+    return table;
+}
+
 async function run(tier) {
     startLoading(3)
     await sleep(3);
@@ -89,6 +121,8 @@ async function run(tier) {
     }
 
     addTabs(finalData, '.tabs-widget .tabs', '.tabs-widget .tabs-content')
+
+    console.log(createTable(result['candidateInfo']));
 }
 
 
