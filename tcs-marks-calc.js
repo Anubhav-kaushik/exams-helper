432f18e23c8100a3c922804a9a5f9bf2dadc8fd5
Anubhav <anubhavsharma313@gmail.com>
2023-08-01 23:24:06 -0700

    update name

--- tcs-marks-calc.js
+++ tcs-marks-calc.js
@@ -1,4 +1,4 @@
-function downloadJson(data) {
+function downloadJson(data, filename='output') {
     // data: dictionary or list
     
     // Convert the JavaScript object to a JSON string
@@ -10,7 +10,7 @@ function downloadJson(data) {
     // Create a temporary link to download the file
     const downloadLink = document.createElement('a');
     downloadLink.href = URL.createObjectURL(blob);
-    downloadLink.download = 'output.json';
+    downloadLink.download = `${filename}.json`;
     
     // Programmatically click the link to trigger the download
     document.body.appendChild(downloadLink);
@@ -424,8 +424,8 @@ function main(page, sectionSelector, sectionNameSelector, mainRowSelector, marki
         'imagesLink': allImages,
     }
 
-    downloadJson(allInfo['answerKeyDict'])
-    downloadJson(allInfo['imagesLink'])
+    downloadJson(allInfo['answerKeyDict'], 'question paper')
+    downloadJson(allInfo['imagesLink'], 'images link')
     // uncomment the above when find a way to save images with json file
 
     return allInfo;
